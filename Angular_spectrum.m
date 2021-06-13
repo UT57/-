65c3d6bc8432405b2bc5b48383db4e8a9b0983ae
UT57 <81698613+UT57@users.noreply.github.com>
2021-06-13 11:29:52 +0300

    Update Angular_spectrum.m

--- Angular_spectrum.m
+++ Angular_spectrum.m
@@ -1,22 +1,24 @@
 %% Задаём поле Z=0
 %Парамеры источника:
 global Lx Ly D f c
-Lx =10;Ly=10; f =10^6; c =1500000;
+Lx =20; Ly=20; f =10^6; c =1000000;
 
 %Расчёт шага сетки,k,lymda:
 global k lymda step
-lymda =c/f; k = 2*pi/lymda; step = lymda/6;
+lymda =c/f; k = 2*pi/lymda; step = lymda/2;
 
 %Задаём поле источника
 global N M
 N = Lx/step;
 M = Ly/step;
 R = N/2;
-IST = zeros(2*N,2*M);
-I1 = 1:2*N; 
-I2 = 1:2*M;
-x = I1-N;                 % mask x-coordinates 
-y = I1-M;                 % mask y-coordinates
+IST = zeros(2*N+1,2*M+1);
+I1 = 1:2*N+1; 
+I2 = 1:2*M+1;
+x1 = -Lx:step:Lx;
+x2 = -Ly:step:Ly;
+x = I1-N-1;                 % mask x-coordinates 
+y = I1-M-1;                 % mask y-coordinates
 [X,Y] = meshgrid(x,y);    % create 2-D mask grid
 A = (X.^2 + Y.^2 <= R^2); % circular aperture of radius R
 IST(A) = 1; 
@@ -25,38 +27,89 @@ IST(A) = 1;
 
 %% УГЛОВОЙ СПЕКТР
 global z 
-z = 20;
+z = 10;
 hh = (pi/Lx)^2;
 %Расчёт углового спектра(БПФ):
-%F =(fft2(IST));
+% F =(fft2(IST));
 F = fftshift(fft2(IST));
-%Домножение на член распространения:
+% pcolor(abs(F));
 
-for n = 1:2*N
-    for m = 1:2*M
-%         kx = (n-N)*pi/Lx;
-%         ky = (m-M)*pi/Ly;
-        S(n,m) = F(n,m)*exp((-1i*z/(2*k))*((n-N)^2+(m-M)^2)*hh);     
+%Домножение на член распространения:
+for n = 1:(2*N+1)
+    for m = 1:(2*M+1)
+        kx = (n-N-1)*pi/Lx;
+        ky = (m-M-1)*pi/Ly;
+        if ((kx^2+ky^2) > k^2 ) 
+            kz = k;
+            F(n,m) = 0;
+        else
+            kz = sqrt(k^2-kx^2-ky^2);
+        end
+
+%        S(n,m) =  F(n,m)*exp(1i*kz*z);
+        prom = 1i*z*(kz);
+       S(n,m) = F(n,m)*exp(prom);
+
     end
 end
-%   pcolor(abs(S));
-%Обратное БПФ:     
- Res =ifft2(S)/4/pi^2;
-% pcolor(abs(Res));
+%    pcolor(abs(S));
 
-%% Обратная задача
-Res_0 = conj(Res);
-Fobr = fftshift(fft2(Res_0));
-%Домножение на член распространения:
+% Обратное БПФ:     
+ Res =ifft2(S);
+  pcolor(abs(Res));
 
-for n = 1:2*N
-    for m = 1:2*M
-        Sobr(n,m) = Fobr(n,m)*exp((-1i*z/(2*k))*((n-N)^2+(m-M)^2)*hh);     
-    end
-end
 
-%Обратное БПФ:     
-Res_obr =ifft2(Sobr)/4/pi^2;
-pcolor(abs(Res_obr));
+% %% ОБРАТНАЯ ЗАДАЧА
+% % Res_0 = conj(Res);
+% Res_0 = Res;
+% Fobr = fftshift(fft2(Res_0));
+% % pcolor (abs(Fobr));
+% 
+% %Домножение на член распространения:
+% for n = 1:2*N+1
+%     for m = 1:2*M+1
+%         kx = (n-N-1)*pi/Lx;
+%         ky = (m-M-1)*pi/Ly;
+%         if ((kx^2+ky^2) > k^2 ) 
+%             kz = k;
+% %             Fobr(n,m) = 0;
+%         else
+%             kz = sqrt(k^2-kx^2-ky^2);
+%         end
+% %         Sobr(n,m) = Fobr(n,m) - S(n,m);
+% %         Sobr(n,m) = Fobr(n,m)*exp(1i*(kz)*z);  
+% %         Sobr(n,m) = exp(1i*(kz)*z);
+%         prom2 = -1i*(kz)*z*0,1;
+%         Sobr(n,m) = Fobr(n,m)*exp(prom2);
+%     end
+% end
+% %   pcolor(abs(Sobr));
+% %Обратное БПФ:     
+% Res_obr =(ifft2(Sobr));
+% pcolor(abs(Res_obr));
 
+
+% 
+% ---------------------------------------
+% 
+% 1) Плохая зависимость обр. задачи от z
+% 2) Разобраться в масштабировании сетки - скорее всего тут проблема
+% 3) Фильтр побочных частот и случайных моментальных волн
+% 4) Работа с погрешностью
+% 5) Разбить все вычесления 
+% --------------------------------------
+
+
+
+
+
+% 
+% %%TEST
+% for n = 1:2*N+1
+%     for m = 1 : 2*M+1
+%         if (abs(S(n,m) - Sobr(n,m)) > 0 )
+%             disp(n);
+%         end
+%     end
+% end
 
