eefd2980dcece959fb3f84ae64723b55cbb57ea0
UT57 <EgorkaEgorovich@yandex.ru>
2022-09-04 03:02:36 +0300

    code format

--- Angular_spectrum.m
+++ Angular_spectrum.m
@@ -1,11 +1,13 @@
-%% Задаём поле Z=0
+%% Задаём поле Z = 0
 %Парамеры источника:
 global Lx Ly D f c
 Lx =20; Ly=20; f =10^6; c =1000000;
 
+
 %Расчёт шага сетки,k,lymda:
 global k lymda step
 lymda =c/f; k = 2*pi/lymda; step = lymda/2;
+
 
 %Задаём поле источника
 global N M
@@ -17,22 +19,26 @@ I1 = 1:2*N+1;
 I2 = 1:2*M+1;
 x1 = -Lx:step:Lx;
 x2 = -Ly:step:Ly;
-x = I1-N-1;                 % mask x-coordinates 
-y = I1-M-1;                 % mask y-coordinates
-[X,Y] = meshgrid(x,y);    % create 2-D mask grid
-A = (X.^2 + Y.^2 <= R^2); % circular aperture of radius R
+x = I1-N-1;                 % x-координаты
+y = I1-M-1;                 % y-координаты
+[X,Y] = meshgrid(x,y);      % задаём плоскую сетку
+A = (X.^2 + Y.^2 <= R^2);   % апертура радиуса R
 IST(A) = 1; 
-%pcolor(IST);
 
+
+%Начальное распределение
+tiledlayout(1,1)
+nexttile
+pcolor(IST); 
+title('Поршневой источник')
 
 %% УГЛОВОЙ СПЕКТР
 global z 
 z = 10;
 hh = (pi/Lx)^2;
 %Расчёт углового спектра(БПФ):
-% F =(fft2(IST));
 F = fftshift(fft2(IST));
-% pcolor(abs(F));
+
 
 %Домножение на член распространения:
 for n = 1:(2*N+1)
@@ -45,18 +51,16 @@ for n = 1:(2*N+1)
         else
             kz = sqrt(k^2-kx^2-ky^2);
         end
-
-%        S(n,m) =  F(n,m)*exp(1i*kz*z);
         prom = 1i*z*(kz)*0,1;
        S(n,m) = F(n,m)*exp(prom);
 
     end
 end
-%    pcolor(abs(S));
+
 
 % Обратное БПФ:     
  Res =ifft2(S);
-%  pcolor(abs(Res));
+
 
 
 %% ОБРАТНАЯ ЗАДАЧА
@@ -64,6 +68,7 @@ end
 Res_0 = Res;
 Fobr = fftshift(fft2(Res_0));
 % pcolor (abs(Fobr));
+
 
 %Домножение на член распространения:
 for n = 1:2*N+1
@@ -87,30 +92,9 @@ end
 
 %Обратное БПФ:     
 Res_obr =(ifft2(Sobr));
+nexttile
 pcolor(abs(Res_obr));
+title('Восстановленный источник')
 
 
-% 
-% ---------------------------------------
-% 
-% 1) Плохая зависимость обр. задачи от z
-% 2) Разобраться в масштабировании сетки - скорее всего тут проблема
-% 3) Фильтр побочных частот и случайных моментальных волн
-% 4) Работа с погрешностью
-% 5) Разбить все вычесления 
-% --------------------------------------
-
-
-
-
-
-% 
-% %%TEST
-% for n = 1:2*N+1
-%     for m = 1 : 2*M+1
-%         if (abs(S(n,m) - Sobr(n,m)) > 0 )
-%             disp(n);
-%         end
-%     end
-% end
 
